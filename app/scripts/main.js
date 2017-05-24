fad6efbf3db655acdf23eeb0e460b261a3adcd47
codewithpassion <dominik.fretz@gmail.com>
2017-05-24 17:09:38 +1000

    Add zoom to galery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,7 +24,7 @@ if (heroSliders.length > 0) {
     });
 }
 
-$('.magnific-gallery').each(function() {
+$('.mfp-gallery').each(function() {
     $(this).magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
@@ -33,5 +33,32 @@ $('.magnific-gallery').each(function() {
             preload: [0, 2], // read about this option in next Lazy-loading section
             navigateByImgClick: true
         }
+    })
+});
+$('.mfp-gallery-zoom').each(function() {
+    $(this).magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        mainClass: 'mfp-with-zoom',
+        gallery: {
+            enabled: true, // set to true to enable gallery
+            preload: [0, 2], // read about this option in next Lazy-loading section
+            navigateByImgClick: true
+        },
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            opener: function (openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.parent().parent().find('img');
+            }
+        },        
     })
 });
