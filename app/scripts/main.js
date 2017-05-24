116194358d074e4cb231b6de91f76bdcd1c3f5b1
codewithpassion <dominik.fretz@gmail.com>
2017-05-25 00:03:31 +1000

    icons on product page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,37 +26,32 @@ if (heroSliders.length > 0) {
 
 $('.mfp-gallery').each(function() {
     $(this).magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
+        delegate: 'a', 
         type: 'image',
         gallery: {
-            enabled: true, // set to true to enable gallery
-            preload: [0, 2], // read about this option in next Lazy-loading section
+            enabled: true, 
+            preload: [0, 2], 
             navigateByImgClick: true
         }
     })
 });
 $('.mfp-gallery-zoom').each(function() {
     $(this).magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
+        delegate: 'a', 
         type: 'image',
         mainClass: 'mfp-with-zoom',
         gallery: {
-            enabled: true, // set to true to enable gallery
-            preload: [0, 2], // read about this option in next Lazy-loading section
+            enabled: true, 
+            preload: [0, 2], 
             navigateByImgClick: true
         },
         zoom: {
-            enabled: true, // By default it's false, so don't forget to enable it
+            enabled: true, 
 
-            duration: 300, // duration of the effect, in milliseconds
-            easing: 'ease-in-out', // CSS transition easing function
+            duration: 300,
+            easing: 'ease-in-out', 
 
-            // The "opener" function should return the element from which popup will be zoomed in
-            // and to which popup will be scaled down
-            // By defailt it looks for an image tag:
             opener: function (openerElement) {
-                // openerElement is the element on which popup was initialized, in this case its <a> tag
-                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
                 return openerElement.is('img') ? openerElement : openerElement.parent().parent().find('img');
             }
         },        
