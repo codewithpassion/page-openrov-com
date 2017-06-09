c9652eebc46003f1d479f960032703a1763358c8
codewithpassion <dominik.fretz@gmail.com>
2017-06-10 02:58:31 +1000

    jump to tech specs

--- app/scripts/trident.js
+++ app/scripts/trident.js
@@ -1,8 +1,31 @@
 (function ($) {
+
+    function scrollToTab(selector) {
+        setTimeout(() => {
+            $('html,body').animate({
+                scrollTop: $(selector).offset().top - 100
+            }, 'slow');
+        }, 250);
+
+    }
+
     $('.carousel').bcSwipe({ threshold: 50 });
     
     $('#feature-list .slide-selector').on('mouseenter', function() {
         $('#feature-list').carousel(parseInt(this.dataset.slideTo));
+    })
+
+    $('a[href="#specs"]').click(function(e) {
+        $('[href="#specs"]').tab('show')
+        scrollToTab('#specs');
+    })
+
+    $(function () {
+        if (!location.hash) return;
+
+        var activeTab = $('[href="' + location.hash + '"]');
+        activeTab && activeTab.tab('show');
+        scrollToTab(location.hash);
     })
 
 })($) 
