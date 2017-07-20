f009eb859ece8949f2772ac26d417cf141f762c3
codewithpassion <dominik.fretz@gmail.com>
2017-07-19 23:33:16 -0700

    gitlab build

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t codewithpassion/www.openrov.com-build . -f Dockerfile.gitlab
+docker build -t codewithpassion/www.openrov.com-build . -f Dockerfile.gitlab && \
 docker push codewithpassion/www.openrov.com-build
